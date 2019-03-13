096176e87eba81694431c153130367cc9193685e
jkeslar21 <diamondroad91@gmail.com>
2019-03-12 21:10:33 -0700

    home

---
+++ jennifer/src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import NavBar from './NavBar';
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div>
+        <NavBar />
+        <img src='https://static1.squarespace.com/static/5b99d7befcf7fda4e9ef7911/t/5bc2425bf4e1fcece83cbca0/1539463803060/Jennifer_Diamond_Actress_Headshot_Black_02_lores.jpg' alt='Actress' />
+      </div>
+    )
+  }
+}
---
+++ jennifer/src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function NavBar() {
+  return (
+    <div>
+      <a href='#'>HOME</a>
+      <a href='#'>BIO</a>
+      <a href='#'>REELS</a>
+      <a href='#'>HEADSHOTS</a>
+      <a href='#'>CONTACT</a>
+    </div>
+  )
+}
